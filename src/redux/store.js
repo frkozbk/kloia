3f526ba1dbafc60a030fc566529c65dc1a152107
Faruk@12 <ozbekfaruk12@gmail.com>
2019-09-04 12:03:50 +0300

    remove redux extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,4 @@
 import { createStore } from "redux";
 import rootReducer from "./rootReducer";
 
-export default createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default createStore(rootReducer);
